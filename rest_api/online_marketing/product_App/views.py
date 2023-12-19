44f8ab810092f5600595b0740b1935da3a7f2517
venkatsai <vdvsaikumar.marolix@gmail.com>
2023-12-19 07:44:52 +0530

    online_marketing

---
+++ rest_api/online_marketing/product_App/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Product
+from .serializers import ProductSerializer
+
+class ProductList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
